d2ddbf6646a94375981f94bd317b473671427629
wangyuyong <Jie1119855061@gmail.com>
2020-05-15 10:15:29 +0800

    完成导航页

---
+++ js/navigation.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $("#club-info").click(function () {
+    //跳转到社团信息界面
+    window.location.href = "/clubInfo.html";
+  });
+
+  $("#publish-recruit").click(function () {
+    //跳转到发布招新信息
+    window.location.href = "/publishRecruitInfo.html";
+  });
+
+  $("#person-info").click(function () {
+    //跳转到发布招新消息
+    window.location.href = "/personalInfo.html";
+  });
+});
